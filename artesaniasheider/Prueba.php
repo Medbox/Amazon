6e379e20c853ebcd0224682c30aba57d7aa4f411
cpavez <capavez88@gmail.com>
2014-05-08 12:54:04 -0400

    no message

--- artesaniasheider/Prueba.php
+++ artesaniasheider/Prueba.php
@@ -2,9 +2,5 @@
 	echo "hoilaaa";
 	echo "modificado por mi";
 	echo "modificado numero 2";
-<<<<<<< HEAD
-	echo "modificado numero 3"
-
-=======
->>>>>>> f4067ae0d2544bd8bdd4a703dc56ff73ad3f685a
+	echo "modificado numero 3";
 ?>
